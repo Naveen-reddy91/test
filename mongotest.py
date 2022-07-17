151c866baf557264ca7a83d7f8b81f1a79d47d1c
User <knvk.reddy@gmail.com>
2022-07-17 19:28:58 +0530

    this is my first commit in Git

--- mongotest.py
+++ mongotest.py
@@ -4,3 +4,25 @@ client = pymongo.MongoClient("mongodb+srv://naveen:09D4@2151@cluster0.o7rzx.mong
 db = client.test
 
 print(db)
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://naveen:09D4@2151@cluster0.o7rzx.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://naveen:09D4@2151@cluster0.o7rzx.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://naveen:09D4@2151@cluster0.o7rzx.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
